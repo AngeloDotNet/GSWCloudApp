a58968f1d1ece09b701632e7ef6137f8bf5f1da5
Angelo Pirola <angelo9871@gmail.com>
2024-11-01 00:17:01 +0100

    Aggiunte Database migrations (ConfigurazioniSvc)

---
+++ src/ConfigurazioniSvc/ConfigurazioniSvc.Migrations/Migrations/20241029120248_InitialMigration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConfigurazioniSvc.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Configurazione",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    festa_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    valore = table.Column<string>(type: "text", nullable: true),
+                    tipo = table.Column<string>(type: "text", nullable: true),
+                    posizione = table.Column<int>(type: "integer", nullable: false),
+                    obbligatorio = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    Scope = table.Column<string>(type: "text", nullable: false),
+                    created_by_user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_date_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    modified_by_user_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    modified_date_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_by_user_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    deleted_date_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_configurazione", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Configurazione");
+        }
+    }
+}
