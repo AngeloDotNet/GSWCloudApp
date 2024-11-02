4c83b3c868c14e467c4d117abb27a8e2231cde30
Angelo Pirola <angelo9871@gmail.com>
2024-11-02 13:57:05 +0100

    Refactoring classLibrary Shared (Configurazioni)

--- src/ConfigurazioniSvc/ConfigurazioniSvc.Shared/DTO/CreateConfigurazioneDto.cs
+++ src/ConfigurazioniSvc/ConfigurazioniSvc.Shared/DTO/CreateConfigurazioneDto.cs
@@ -5,6 +5,7 @@ namespace ConfigurazioniSvc.Shared.DTO;
 public class CreateConfigurazioneDto
 {
     public Guid FestaId { get; set; }
+    public string Chiave { get; set; } = null!;
     public string Valore { get; set; } = null!;
     public string Tipo { get; set; } = null!;
     public int Posizione { get; set; }
