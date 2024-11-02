a78834893bb6a7e0efb685b2060f7b661f17b0e9
Angelo Pirola <angelo9871@gmail.com>
2024-11-02 13:58:00 +0100

    Aggiunta classlibrary DataAccessLayer (Configurazioni)

---
+++ src/ConfigurazioniSvc/ConfigurazioniSvc.DataAccessLayer/Entities/Configurazione.cs
@@ -0,0 +1,15 @@
+﻿using ConfigurazioniSvc.DataAccessLayer.Entities.Common;
+using ConfigurazioniSvc.Shared.Enums;
+
+namespace ConfigurazioniSvc.DataAccessLayer.Entities;
+
+public class Configurazione : SoftDeletableEntity<Guid>
+{
+    public Guid FestaId { get; set; }
+    public string Chiave { get; set; } = null!;
+    public string Valore { get; set; } = null!;
+    public string Tipo { get; set; } = null!;
+    public int Posizione { get; set; }
+    public bool Obbligatorio { get; set; }
+    public ScopoConfigurazione Scope { get; set; }
+}
