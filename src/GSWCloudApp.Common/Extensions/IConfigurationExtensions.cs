b6dd0cdf5df2590f91aadbfa7437c253471b9266
Angelo Pirola <angelo9871@gmail.com>
2024-11-06 09:00:54 +0100

    Aggiunta configurazione Extension method

---
+++ src/GSWCloudApp.Common/Extensions/IConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GSWCloudApp.Common.Extensions;
+
+public static class IConfigurationExtensions
+{
+    public static T? ConfigureAndGet<T>(this IServiceCollection services, IConfiguration configuration, string sectionName) where T : class
+    {
+        var section = configuration.GetSection(sectionName);
+        var settings = section.Get<T>();
+
+        services.Configure<T>(section);
+
+        return settings;
+    }
+}
