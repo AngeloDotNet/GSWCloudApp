ca740f3fac46d9d06350684d91cf65ec92c88ddf
Angelo Pirola <angelo9871@gmail.com>
2024-11-06 09:01:11 +0100

    Aggiunta configurazione Routing

---
+++ src/GSWCloudApp.Common/Routing/IEndpointRouteHandlerBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace GSWCloudApp.Common.Routing;
+
+public interface IEndpointRouteHandlerBuilder
+{
+    public static abstract void MapEndpoints(IEndpointRouteBuilder endpoints);
+}
