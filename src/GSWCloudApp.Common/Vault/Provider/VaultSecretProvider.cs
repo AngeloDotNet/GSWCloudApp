203511e5d55e2945e37bd189e02e7edfbfbb2586
Angelo Pirola <angelo9871@gmail.com>
2024-11-07 09:59:08 +0100

    Aggiunta configurazione Vault

---
+++ src/GSWCloudApp.Common/Vault/Provider/VaultSecretProvider.cs
@@ -0,0 +1,42 @@
+﻿using GSWCloudApp.Common.Vault.Options;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+
+namespace GSWCloudApp.Common.Vault.Provider;
+
+public class VaultSecretProvider
+{
+    private readonly IVaultClient vaultClient;
+
+    public VaultSecretProvider(string vaultUri, string vaultToken)
+    {
+        var authMethod = new TokenAuthMethodInfo(vaultToken);
+        var vaultClientSettings = new VaultClientSettings(vaultUri, authMethod);
+
+        vaultClient = new VaultClient(vaultClientSettings);
+    }
+
+    public async Task<string> GetSecretValueAsync(VaultOptions vaultSettings, string path, string key)
+    {
+        var secret = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: path, mountPoint: vaultSettings.MountPoint);
+
+        if (secret.Data.Data.TryGetValue(key, out var value))
+        {
+            return value.ToString()!;
+        }
+
+        throw new KeyNotFoundException($"Key '{key}' not found in Vault at path '{path}'");
+    }
+
+    public async Task<bool> SetSecretValueAsync(string path, string key, string value, string mountPoint)
+    {
+        var secret = new Dictionary<string, object>
+        {
+            { key, value }
+        };
+
+        var writeSecret = await vaultClient.V1.Secrets.KeyValue.V2.WriteSecretAsync(path: path, secret, checkAndSet: null, mountPoint: mountPoint);
+
+        return true;
+    }
+}
---
+++ src/GSWCloudApp.Common/Vault/Service/VaultService.cs
@@ -0,0 +1,23 @@
+﻿using GSWCloudApp.Common.Vault.Options;
+using GSWCloudApp.Common.Vault.Provider;
+
+namespace GSWCloudApp.Common.Vault.Service;
+
+public static class VaultService
+{
+    public static async Task<bool> WriteVaultSecretAsync(VaultOptions vaultSettings, string path, string key, string value, string mountPoint)
+    {
+        var vaultSecretsProvider = new VaultSecretProvider(vaultSettings!.Address, vaultSettings.Token);
+
+        var isCreated = await vaultSecretsProvider.SetSecretValueAsync(path: path, key: key, value: value, mountPoint: mountPoint);
+
+        return isCreated;
+    }
+
+    public static async Task<string> ReadVaultSecretAsync(VaultOptions vaultSettings, string path, string key)
+    {
+        var vaultSecretsProvider = new VaultSecretProvider(vaultSettings!.Address, vaultSettings.Token);
+
+        return await vaultSecretsProvider.GetSecretValueAsync(vaultSettings, path: path, key: key);
+    }
+}
