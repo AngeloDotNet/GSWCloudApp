63e808af07953c54b11654bd82df78c4b0d7a38d
Angelo Pirola <angelo9871@gmail.com>
2024-11-07 09:58:57 +0100

    Aggiunta configurazione Validazione

---
+++ src/GSWCloudApp.Common/Validation/ValidationOptions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace GSWCloudApp.Common.Validation;
+
+public class ValidationOptions
+{
+    public ErrorResponseFormat ErrorResponseFormat { get; set; }
+    public Func<EndpointFilterInvocationContext, IDictionary<string, string[]>, string>? ValidationErrorTitleMessageFactory { get; set; }
+}
