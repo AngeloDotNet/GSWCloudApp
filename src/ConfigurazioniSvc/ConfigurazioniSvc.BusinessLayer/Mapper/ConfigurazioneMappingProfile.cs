313312268743ec7d09f91e851dc8a705b7c83801
Angelo Pirola <angelo9871@gmail.com>
2024-11-10 10:05:22 +0100

    Aggiunta configurazione BusinessLayer ConfigurazioniSvc

---
+++ src/ConfigurazioniSvc/ConfigurazioniSvc.BusinessLayer/Mapper/ConfigurazioneMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ConfigurazioniSvc.DataAccessLayer.Entities;
+using ConfigurazioniSvc.Shared.DTO;
+
+namespace ConfigurazioniSvc.BusinessLayer.Mapper;
+
+public class ConfigurazioneMappingProfile : Profile
+{
+    public ConfigurazioneMappingProfile()
+    {
+        CreateMap<Configurazione, ConfigurazioneDto>().ReverseMap();
+        CreateMap<CreateConfigurazioneDto, Configurazione>().ReverseMap();
+
+        CreateMap<Configurazione, EditConfigurazioneDto>();
+        CreateMap<EditConfigurazioneDto, Configurazione>()
+            .ForMember(dst => dst.FestaId, opt => opt.PreCondition(src => src.FestaId != null))
+            .ForMember(dst => dst.Chiave, opt => opt.PreCondition(src => src.Chiave != null))
+            .ForMember(dst => dst.Valore, opt => opt.PreCondition(src => src.Valore != null))
+            .ForMember(dst => dst.Tipo, opt => opt.PreCondition(src => src.Tipo != null))
+            .ForMember(dst => dst.Posizione, opt => opt.PreCondition(src => src.Posizione != null))
+            .ForMember(dst => dst.Obbligatorio, opt => opt.PreCondition(src => src.Obbligatorio != null))
+            .ForMember(dst => dst.Scope, opt => opt.PreCondition(src => src.Scope != null));
+    }
+}
---
+++ src/ConfigurazioniSvc/ConfigurazioniSvc.BusinessLayer/Validation/ConfigurazioneValidator.cs
@@ -0,0 +1,28 @@
+﻿using ConfigurazioniSvc.Shared.DTO;
+using FluentValidation;
+
+namespace ConfigurazioniSvc.BusinessLayer.Validation;
+
+public class CreateConfigurazioneValidator : AbstractValidator<CreateConfigurazioneDto>
+{
+    public CreateConfigurazioneValidator()
+    {
+        RuleFor(x => x.FestaId)
+            .NotEmpty();
+
+        RuleFor(x => x.Chiave)
+            .NotEmpty();
+
+        RuleFor(x => x.Valore)
+            .NotEmpty();
+
+        RuleFor(x => x.Tipo)
+            .NotEmpty();
+
+        RuleFor(x => x.Scope)
+            .IsInEnum();
+    }
+}
+
+public class EditConfigurazioneValidator : AbstractValidator<EditConfigurazioneDto>
+{ }
