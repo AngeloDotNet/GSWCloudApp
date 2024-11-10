630390847adefcf0e44ba4de0c83553d4045f6dc
Angelo Pirola <angelo9871@gmail.com>
2024-11-10 10:05:39 +0100

    Aggiunta configurazione API ConfigurazioniSvc

---
+++ src/ConfigurazioniSvc/ConfigurazioniSvc/Helpers/ApplicationHelpers.cs
@@ -0,0 +1,52 @@
+﻿using ConfigurazioniSvc.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ConfigurazioniSvc.Helpers;
+
+public static class ApplicationHelpers
+{
+    public static async Task ConfigureDatabaseAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (!dbContext.Database.IsInMemory())
+        {
+            await EnsureDatabaseAsync();
+            await RunMigrationsAsync();
+        }
+        else
+        {
+            dbContext.Database.EnsureCreated();
+        }
+
+        async Task EnsureDatabaseAsync()
+        {
+            var dbCreator = dbContext.GetService<IRelationalDatabaseCreator>();
+            var strategy = dbContext.Database.CreateExecutionStrategy();
+
+            await strategy.ExecuteAsync(async () =>
+            {
+                if (!await dbCreator.ExistsAsync())
+                {
+                    await dbCreator.CreateAsync();
+                }
+            });
+        }
+
+        async Task RunMigrationsAsync()
+        {
+            var strategy = dbContext.Database.CreateExecutionStrategy();
+
+            await strategy.ExecuteAsync(async () =>
+            {
+                await using var transaction = await dbContext.Database.BeginTransactionAsync();
+
+                await dbContext.Database.MigrateAsync();
+                await transaction.CommitAsync();
+            });
+        }
+    }
+}
