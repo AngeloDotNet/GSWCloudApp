8b8452da2fe69da8feb3e59844a7b97558a28128
Angelo Pirola <angelo9871@gmail.com>
2024-11-18 00:13:24 +0100

    Aggiunta nuova implementazione - Entities

---
+++ src/GSWCloudApp.Common/Entities/BaseEntityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GSWCloudApp.Common.Entities;
+
+public abstract class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T> where T : BaseEntity<Guid>
+{
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        builder.HasKey(x => x.Id);
+    }
+}
