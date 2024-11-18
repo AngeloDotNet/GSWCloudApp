d363b90be5696cd14bdb5984115262954afe16ed
Angelo Pirola <angelo9871@gmail.com>
2024-11-19 00:08:31 +0100

    Refactoring Extensions

--- src/GSWCloudApp.Common/Extensions/ApplicationExtensions.cs
+++ src/GSWCloudApp.Common/Extensions/ApplicationExtensions.cs
@@ -1,12 +1,37 @@
-﻿using GSWCloudApp.Common.Options;
+﻿using Asp.Versioning;
+using GSWCloudApp.Common.Options;
+using GSWCloudApp.Common.Vault.Options;
+using GSWCloudApp.Common.Vault.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Hosting;
 
 namespace GSWCloudApp.Common.Extensions;
 
 public static class ApplicationExtensions
 {
+    public static async Task<string> GetVaultStringConnectionAsync(WebApplicationBuilder builder, string vaultPath, string vaultKey)
+    {
+        var vaultOptions = builder.Services.ConfigureAndGet<VaultOptions>(builder.Configuration, nameof(VaultOptions))
+            ?? throw new InvalidOperationException("Vault options not found.");
+
+        return await VaultService.ReadVaultSecretAsync(vaultOptions, vaultPath, vaultKey);
+    }
+
+    public static RouteGroupBuilder UseVersioningApi(WebApplication app)
+    {
+        var apiVersionSet = app.NewApiVersionSet()
+            .HasApiVersion(new ApiVersion(1))
+            .Build();
+
+        var versionedApi = app
+            .MapGroup("/api/v{version:apiVersion}")
+            .WithApiVersionSet(apiVersionSet);
+
+        return versionedApi;
+    }
+
     public static void UseDevSwagger(this WebApplication app, ApplicationOptions options)
     {
         if (app.Environment.IsDevelopment() || options.SwaggerEnable)
