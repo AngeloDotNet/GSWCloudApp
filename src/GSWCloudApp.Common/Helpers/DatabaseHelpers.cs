b6208bda9ce607761b9ec710f19601d0f05ae712
Angelo Pirola <angelo9871@gmail.com>
2024-11-19 00:08:42 +0100

    Refactoring Helpers

---
+++ src/GSWCloudApp.Common/Helpers/DatabaseHelpers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GSWCloudApp.Common.Helpers;
+
+public static class DatabaseHelpers
+{
+    public static void ExecuteSQLScriptFromAssembly(this MigrationBuilder migrationBuilder, string assetName)
+    {
+        var sqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SQLScripts", assetName);
+        migrationBuilder.Sql(File.ReadAllText(sqlFile));
+    }
+}
