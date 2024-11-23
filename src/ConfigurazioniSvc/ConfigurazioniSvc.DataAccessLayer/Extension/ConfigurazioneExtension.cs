e80555c7a53292f37453ab015228dd7f5c312529
Angelo Pirola <angelo9871@gmail.com>
2024-11-24 00:50:21 +0100

    Refactoring Extensions + DbContext

--- src/ConfigurazioniSvc/ConfigurazioniSvc.DataAccessLayer/Extension/ConfigurazioneExtension.cs
+++ src/ConfigurazioniSvc/ConfigurazioniSvc.DataAccessLayer/Extension/ConfigurazioneExtension.cs
@@ -1,28 +1,19 @@
 ﻿using ConfigurazioniSvc.DataAccessLayer.Entities;
+using GSWCloudApp.Common.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ConfigurazioniSvc.DataAccessLayer.Extension;
 
-public class ConfigurazioneExtension : IEntityTypeConfiguration<Configurazione>
+public class ConfigurazioneExtension : BaseEntityConfiguration<Configurazione>
 {
-    public void Configure(EntityTypeBuilder<Configurazione> builder)
+    public override void Configure(EntityTypeBuilder<Configurazione> builder)
     {
         builder.ToTable("Configurazione");
-        builder.HasKey(builder => builder.Id);
+        builder.HasIndex(builder => builder.Chiave).IsUnique();
 
-        builder.Property(builder => builder.Id)
-            .ValueGeneratedOnAdd();
-
-        builder.HasIndex(builder => builder.Chiave)
-            .IsUnique();
-
-        builder.Property(builder => builder.Obbligatorio)
-            .HasDefaultValue(false);
-
-        builder.Property(builder => builder.Scope)
-            .HasConversion<string>()
-            .HasColumnName("Scope");
+        builder.Property(builder => builder.Obbligatorio).HasDefaultValue(false);
+        builder.Property(builder => builder.Scope).HasConversion<string>().HasColumnName("Scope");
 
         builder.HasQueryFilter(builder => builder.DeletedDateTime == null);
     }
