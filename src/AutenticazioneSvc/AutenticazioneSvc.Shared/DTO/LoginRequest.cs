82bb85ba9e869719a1a2634cc121bb3e477d17c0
Angelo Pirola <angelo9871@gmail.com>
2024-12-02 00:02:15 +0100

    Primo commit AutenticazioneSvc Shared

---
+++ src/AutenticazioneSvc/AutenticazioneSvc.Shared/DTO/LoginRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace AutenticazioneSvc.Shared.DTO;
+
+public class LoginRequest
+{
+    public string UserName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
---
+++ src/AutenticazioneSvc/AutenticazioneSvc.Shared/DTO/RegisterResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace AutenticazioneSvc.Shared.DTO;
+
+public class RegisterResponse
+{
+    public bool Succeeded { get; set; }
+    public IEnumerable<string> Errors { get; set; } = [];
+}
