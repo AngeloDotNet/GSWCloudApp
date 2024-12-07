8e4a22903515b4c53a8108945c4dcb061916165b
Angelo Pirola <angelo9871@gmail.com>
2024-12-08 00:02:03 +0100

    Aggiunta configurazione condivisa per Identity

---
+++ src/GSWCloudApp.Common/Identity/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace GSWCloudApp.Common.Identity.Extensions;
+
+public static class ClaimsExtensions
+{
+    public static Guid GetId(this IPrincipal user)
+    {
+        var value = user.GetClaimValue(ClaimTypes.NameIdentifier);
+        return Guid.Parse(value);
+    }
+
+    public static string GetClaimValue(this IPrincipal user, string claimType)
+    {
+        var value = ((ClaimsPrincipal)user).FindFirst(claimType)?.Value;
+
+        return value!;
+    }
+}
