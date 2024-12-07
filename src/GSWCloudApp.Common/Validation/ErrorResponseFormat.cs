f96d6c62cf37eabaab4bc5120c44dd7ae204f1eb
Angelo Pirola <angelo9871@gmail.com>
2024-12-08 00:02:48 +0100

    Aggiunti commenti

--- src/GSWCloudApp.Common/Validation/ErrorResponseFormat.cs
+++ src/GSWCloudApp.Common/Validation/ErrorResponseFormat.cs
@@ -1,7 +1,17 @@
 ﻿namespace GSWCloudApp.Common.Validation;
 
+/// <summary>
+/// Specifies the format of the error response.
+/// </summary>
 public enum ErrorResponseFormat
 {
+    /// <summary>
+    /// The default error response format.
+    /// </summary>
     Default,
+
+    /// <summary>
+    /// The error response format as a list.
+    /// </summary>
     List
 }
