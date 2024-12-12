733e1f04f434fcace57dc80716ce7b805da0fa0d
Angelo Pirola <angelo9871@gmail.com>
2024-12-13 00:04:04 +0100

    Fix BusinessLayer

--- src/AutenticazioneSvc/AutenticazioneSvc.BusinessLayer/HostedService/AuthStartupTask.cs
+++ src/AutenticazioneSvc/AutenticazioneSvc.BusinessLayer/HostedService/AuthStartupTask.cs
@@ -1,4 +1,5 @@
-﻿using GSWCloudApp.Common.Identity.Entities;
+﻿using System.Security.Cryptography;
+using GSWCloudApp.Common.Identity.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,10 +15,59 @@ public class AuthStartupTask(IServiceProvider serviceProvider) : IHostedService
         var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
         await GenerateRolesAsync(roleManager);
 
-        //TODO: Manca la generazione dell'utente amministratore
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        await GenerateAdminUserProfileAsync(scope, userManager);
     }
 
-    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    private static async Task GenerateAdminUserProfileAsync(IServiceScope scope, UserManager<ApplicationUser> userManager)
+    {
+        var administratorUser = new ApplicationUser
+        {
+            UserName = "admin",
+            Email = "admin@example.com",
+            FirstName = "Admin",
+            LastName = "User",
+            EmailConfirmed = true,
+            TwoFactorEnabled = false,
+            LockoutEnabled = false
+        };
+
+        //TODO: Aggiungere un log per loggare la password generata
+        await CheckCreateUserAsync(administratorUser, GeneratePassword(15), RoleNames.Administrator);
+
+        async Task CheckCreateUserAsync(ApplicationUser user, string password, params string[] roles)
+        {
+            var dbUser = await userManager.FindByEmailAsync(user.Email!);
+
+            if (dbUser == null)
+            {
+                var result = await userManager.CreateAsync(user, password);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRolesAsync(user, roles);
+                }
+            }
+        }
+    }
+
+    private static string GeneratePassword(int length)
+    {
+        var validCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$&".ToCharArray();
+
+        if (length <= 0)
+        {
+            throw new ArgumentException("Password length must be greater than zero.", nameof(length));
+        }
+
+        var byteBuffer = new byte[length];
+
+        RandomNumberGenerator.Fill(byteBuffer);
+
+        var passwordChars = byteBuffer.Select(b => validCharacters[b % validCharacters.Length]).ToArray();
+
+        return new string(passwordChars);
+    }
 
     private static async Task GenerateRolesAsync(RoleManager<ApplicationRole> roleManager)
     {
@@ -33,4 +83,6 @@ public class AuthStartupTask(IServiceProvider serviceProvider) : IHostedService
             }
         }
     }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 }
