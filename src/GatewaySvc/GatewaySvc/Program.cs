417aa32d701b2aac6510b9ad8144221dff6f8d33
Angelo Pirola <angelo9871@gmail.com>
2024-12-16 19:43:22 +0100

    Fix 2 Ocelot Gateway

--- src/GatewaySvc/GatewaySvc/Program.cs
+++ src/GatewaySvc/GatewaySvc/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 namespace GatewaySvc;
@@ -8,13 +9,16 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        //builder.Configuration.AddOcelot("ConfigOcelot", builder.Environment);
         var configFiles = Directory.GetFiles("ConfigOcelot", "*.json");
 
         foreach (var configFile in configFiles)
         {
             builder.Configuration.AddJsonFile(configFile, optional: false, reloadOnChange: true);
         }
+
+        //builder.Configuration.AddJsonFile("ConfigOcelot/ocelot.configurazioni.json", optional: false, reloadOnChange: true);
+        //builder.Configuration.AddJsonFile("ConfigOcelot/ocelot.categorie.json", optional: false, reloadOnChange: true);
+        builder.Services.AddOcelot();
 
         builder.Services.Configure<KestrelServerOptions>(builder.Configuration.GetSection("Kestrel"));
         builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
