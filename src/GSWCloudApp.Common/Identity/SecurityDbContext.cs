dcb38a58839f36928ec71af561c56bb7accd3972
Angelo Pirola <angelo9871@gmail.com>
2024-12-17 20:17:09 +0100

    Aggiunto DbContext Identity

---
+++ src/GSWCloudApp.Common/Identity/SecurityDbContext.cs
@@ -0,0 +1,10 @@
+﻿using GSWCloudApp.Common.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GSWCloudApp.Common.Identity;
+
+public class SecurityDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser, ApplicationRole, Guid,
+    IdentityUserClaim<Guid>, ApplicationUserRole, IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, IdentityUserToken<Guid>>(options)
+{ }
