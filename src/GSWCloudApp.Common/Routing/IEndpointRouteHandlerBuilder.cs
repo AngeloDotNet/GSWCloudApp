558dac32151393314ac4f7be047a98115fc9b9a8
Angelo Pirola <angelo9871@gmail.com>
2024-12-19 12:06:34 +0100

    Aggiunta documentazione Routing

--- src/GSWCloudApp.Common/Routing/IEndpointRouteHandlerBuilder.cs
+++ src/GSWCloudApp.Common/Routing/IEndpointRouteHandlerBuilder.cs
@@ -2,7 +2,14 @@
 
 namespace GSWCloudApp.Common.Routing;
 
+/// <summary>
+/// Defines a contract for mapping endpoints.
+/// </summary>
 public interface IEndpointRouteHandlerBuilder
 {
+    /// <summary>
+    /// Maps endpoints using the specified endpoint route builder.
+    /// </summary>
+    /// <param name="endpoints">The endpoint route builder.</param>
     public static abstract void MapEndpoints(IEndpointRouteBuilder endpoints);
 }
