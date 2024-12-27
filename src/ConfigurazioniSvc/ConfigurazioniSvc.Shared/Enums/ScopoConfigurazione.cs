0e365a8d2fa75b3c101a0ca7aabb721a72386a5f
Angelo Pirola <angelo9871@gmail.com>
2024-12-27 10:48:29 +0100

    Refactoring ConfigurazioneSvc Shared

--- src/ConfigurazioniSvc/ConfigurazioniSvc.Shared/Enums/ScopoConfigurazione.cs
+++ src/ConfigurazioniSvc/ConfigurazioniSvc.Shared/Enums/ScopoConfigurazione.cs
@@ -1,17 +1,7 @@
 ﻿namespace ConfigurazioniSvc.Shared.Enums;
 
-/// <summary>
-/// Specifies the scope of the configuration.
-/// </summary>
 public enum ScopoConfigurazione
 {
-    /// <summary>
-    /// No specific scope.
-    /// </summary>
     None,
-
-    /// <summary>
-    /// Scope for SMTP server configuration.
-    /// </summary>
     SmtpServer
 }
