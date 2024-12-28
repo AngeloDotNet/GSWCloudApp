018eeb008e8fbd0ceaad985db29d083e078d29fc
Angelo Pirola <angelo9871@gmail.com>
2024-12-28 09:51:38 +0100

    Refactoring ConfigurazioneSvc BusinessLayer

--- src/ConfigurazioniSvc/ConfigurazioniSvc.BusinessLayer/Mapper/MappingProfile.cs
+++ src/ConfigurazioniSvc/ConfigurazioniSvc.BusinessLayer/Mapper/MappingProfile.cs
@@ -4,14 +4,8 @@ using ConfigurazioniSvc.Shared.DTO;
 
 namespace ConfigurazioniSvc.BusinessLayer.Mapper;
 
-/// <summary>
-/// Defines the mapping profile for AutoMapper.
-/// </summary>
 public class MappingProfile : Profile
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="MappingProfile"/> class.
-    /// </summary>
     public MappingProfile()
     {
 
@@ -20,12 +14,32 @@ public class MappingProfile : Profile
 
         CreateMap<Configurazione, EditConfigurazioneDto>();
         CreateMap<EditConfigurazioneDto, Configurazione>()
-            .ForMember(dst => dst.FestaId, opt => opt.PreCondition(src => src.FestaId != null))
-            .ForMember(dst => dst.Chiave, opt => opt.PreCondition(src => src.Chiave != null))
-            .ForMember(dst => dst.Valore, opt => opt.PreCondition(src => src.Valore != null))
-            .ForMember(dst => dst.Tipo, opt => opt.PreCondition(src => src.Tipo != null))
-            .ForMember(dst => dst.Posizione, opt => opt.PreCondition(src => src.Posizione != null))
-            .ForMember(dst => dst.Obbligatorio, opt => opt.PreCondition(src => src.Obbligatorio != null))
-            .ForMember(dst => dst.Scope, opt => opt.PreCondition(src => src.Scope != null));
+            .ForMember(dst => dst.FestaId, opt
+                => opt.PreCondition(src
+                    => src.FestaId != null))
+
+            .ForMember(dst => dst.Chiave, opt
+                => opt.PreCondition(src
+                    => src.Chiave != null))
+
+            .ForMember(dst => dst.Valore, opt
+                => opt.PreCondition(src
+                    => src.Valore != null))
+
+            .ForMember(dst => dst.Tipo, opt
+                => opt.PreCondition(src
+                    => src.Tipo != null))
+
+            .ForMember(dst => dst.Posizione, opt
+                => opt.PreCondition(src
+                    => src.Posizione != null))
+
+            .ForMember(dst => dst.Obbligatorio, opt
+                => opt.PreCondition(src
+                    => src.Obbligatorio != null))
+
+            .ForMember(dst => dst.Scope, opt
+                => opt.PreCondition(src
+                    => src.Scope != null));
     }
 }
