e4ec13dca2113c1bccb00e53999ed74e15daca33
Angelo Pirola <angelo9871@gmail.com>
2024-12-29 00:58:13 +0100

    Refactoring

--- src/ConfigurazioniSvc/ConfigurazioniSvc.LoadBalancer/Program.cs
+++ src/ConfigurazioniSvc/ConfigurazioniSvc.LoadBalancer/Program.cs
@@ -2,13 +2,13 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace ConfigurazioniSvc.LoadBalancer;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddReverseProxy()
-            .LoadFromConfig(builder.Configuration.GetSection("LoadBalancer"));
+
+        builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("LoadBalancer"));
 
         builder.Services.Configure<KestrelServerOptions>(builder.Configuration.GetSection("Kestrel"));
         builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
