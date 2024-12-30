6635caf1ff31c6cefa12e0efbfa56d79b4af7daf
Angelo Pirola <angelo9871@gmail.com>
2024-12-30 09:53:43 +0100

    Aggiunta configurazione Exceptions

---
+++ src/GSWCloudApp.Common/Exceptions/DatabaseInvalidException.cs
@@ -0,0 +1,12 @@
+﻿namespace GSWCloudApp.Common.Exceptions;
+
+public class DatabaseInvalidException : Exception
+{
+    public DatabaseInvalidException(string message) : base(message)
+    {
+    }
+
+    public DatabaseInvalidException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
