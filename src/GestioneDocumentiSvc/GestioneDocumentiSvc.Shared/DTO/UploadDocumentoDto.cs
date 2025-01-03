c21cc78bf2a2f79e9e218d6f4ce66fb05e3a7164
Angelo Pirola <angelo9871@gmail.com>
2025-01-03 11:09:47 +0100

    Aggiunta class library Shared GestioneDocumenti

---
+++ src/GestioneDocumentiSvc/GestioneDocumentiSvc.Shared/DTO/UploadDocumentoDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GestioneDocumentiSvc.Shared.DTO;
+
+public class UploadDocumentoDto
+{
+    [BindRequired]
+    public IFormFile Documento { get; set; }
+
+    public Guid FestaId { get; set; }
+    public string NomeDocumento { get; set; } = null!;
+    public string Descrizione { get; set; } = null!;
+}
