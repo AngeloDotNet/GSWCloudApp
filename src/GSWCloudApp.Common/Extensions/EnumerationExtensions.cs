ea4f9fba451842274d5915dbb8bb0f8ac06d192a
Angelo Pirola <angelo9871@gmail.com>
2025-02-01 00:42:21 +0100

    Aggiunta nuova extension

---
+++ src/GSWCloudApp.Common/Extensions/EnumerationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace GSWCloudApp.Common.Extensions;
+
+public static class EnumerationExtensions
+{
+    public static string GetDescription(this Enum value)
+    {
+        var enumString = value.ToString();
+        var fi = value.GetType().GetField(enumString)!;
+        var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+        return attributes.Length != 0 ? attributes[0].Description : enumString;
+    }
+}
