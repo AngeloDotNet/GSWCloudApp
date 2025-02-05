2441c13e93201b40bf3946aabeebf4ed043b8d2a
Angelo Pirola <angelo9871@gmail.com>
2025-02-06 00:07:13 +0100

    Aggiunto retry policy Polly

---
+++ src/GSWCloudApp.Common/Polly/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
+
+namespace GSWCloudApp.Common.Polly;
+
+public static class DependencyInjection
+{
+    public static AsyncRetryPolicy GetRetryPolicy(ILogger logger)
+    {
+        return Policy.Handle<Exception>().WaitAndRetryAsync(
+            retryCount: 3, // Numero di tentativi
+            sleepDurationProvider: attempt => TimeSpan.FromSeconds(Math.Pow(5, attempt)), // Intervallo esponenziale
+            onRetry: (exc, timespan, attempt, context)
+            => logger.LogWarning(exc, "Tentativo {Attempt} fallito. Riprovo tra {TimespanSeconds} secondi.", attempt, timespan.TotalSeconds));
+    }
+}
