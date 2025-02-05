cbbcdf9ebe988324610115e4541b3c4e57138ec2
Angelo Pirola <angelo9871@gmail.com>
2025-02-06 00:06:14 +0100

    Aggiunti settings Invio email

---
+++ src/GSWCloudApp.Common/Settings/SendEmail/SenderSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace GSWCloudApp.Common.Settings.SendEmail;
+
+/// <summary>
+/// Represents the settings for the email sender.
+/// </summary>
+public class SenderSettings
+{
+    /// <summary>
+    /// Gets or sets the name of the sender.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the email address of the sender.
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+}
