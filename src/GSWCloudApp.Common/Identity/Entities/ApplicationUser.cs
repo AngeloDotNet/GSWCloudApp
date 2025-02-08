db0c932c644a7d4b2ae01f028e2195e21175eb7e
Angelo Pirola <angelo9871@gmail.com>
2025-02-08 01:10:29 +0100

    Refactoring Common Entities

--- src/GSWCloudApp.Common/Identity/Entities/ApplicationUser.cs
+++ src/GSWCloudApp.Common/Identity/Entities/ApplicationUser.cs
@@ -4,9 +4,28 @@ namespace GSWCloudApp.Common.Identity.Entities;
 
 public class ApplicationUser : IdentityUser<Guid>
 {
+    /// <summary>
+    /// Gets or sets the first name of the user.
+    /// </summary>
     public string FirstName { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the last name of the user.
+    /// </summary>
     public string LastName { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the refresh token for the user.
+    /// </summary>
     public string? RefreshToken { get; set; }
+
+    /// <summary>
+    /// Gets or sets the expiration date of the refresh token.
+    /// </summary>
     public DateTime? RefreshTokenExpirationDate { get; set; }
+
+    /// <summary>
+    /// Gets or sets the collection of user roles associated with this user.
+    /// </summary>
     public virtual ICollection<ApplicationUserRole> UserRoles { get; set; } = [];
 }
--- src/GSWCloudApp.Common/Identity/Entities/ApplicationUserRole.cs
+++ src/GSWCloudApp.Common/Identity/Entities/ApplicationUserRole.cs
@@ -4,6 +4,13 @@ namespace GSWCloudApp.Common.Identity.Entities;
 
 public class ApplicationUserRole : IdentityUserRole<Guid>
 {
+    /// <summary>
+    /// Gets or sets the user associated with this user role.
+    /// </summary>
     public virtual ApplicationUser User { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the role associated with this user role.
+    /// </summary>
     public virtual ApplicationRole Role { get; set; } = null!;
 }
