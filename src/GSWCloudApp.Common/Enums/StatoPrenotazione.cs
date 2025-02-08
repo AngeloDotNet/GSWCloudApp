46deb45535a00462dcd6be183ca9a03cd40036b4
Angelo Pirola <angelo9871@gmail.com>
2025-02-08 01:10:05 +0100

    Refactoring Common Enums

--- src/GSWCloudApp.Common/Enums/StatoPrenotazione.cs
+++ src/GSWCloudApp.Common/Enums/StatoPrenotazione.cs
@@ -2,17 +2,32 @@
 
 namespace GSWCloudApp.Common.Enums;
 
+/// <summary>
+/// Enum representing the different states of a reservation.
+/// </summary>
 public enum StatoPrenotazione
 {
+    /// <summary>
+    /// Reservation has been inserted.
+    /// </summary>
     [Description("Inserita")]
     Stato1,
 
+    /// <summary>
+    /// Reservation has been confirmed.
+    /// </summary>
     [Description("Confermata")]
     Stato2,
 
+    /// <summary>
+    /// Reservation is awaiting confirmation.
+    /// </summary>
     [Description("In attesa di conferma")]
     Stato3,
 
+    /// <summary>
+    /// Reservation has been canceled.
+    /// </summary>
     [Description("Annullata")]
     Stato4
 }
