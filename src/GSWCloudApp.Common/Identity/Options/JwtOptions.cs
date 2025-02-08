656272201066e83aaf97a39b339d4a3a86534782
Angelo Pirola <angelo9871@gmail.com>
2025-02-08 01:10:46 +0100

    Refactoring Common Options

--- src/GSWCloudApp.Common/Identity/Options/JwtOptions.cs
+++ src/GSWCloudApp.Common/Identity/Options/JwtOptions.cs
@@ -2,9 +2,28 @@
 
 public class JwtOptions
 {
+    /// <summary>
+    /// Gets the issuer of the JWT.
+    /// </summary>
     public string Issuer { get; init; } = null!;
+
+    /// <summary>
+    /// Gets the audience of the JWT.
+    /// </summary>
     public string Audience { get; init; } = null!;
+
+    /// <summary>
+    /// Gets the security key used to sign the JWT.
+    /// </summary>
     public string SecurityKey { get; init; } = null!;
+
+    /// <summary>
+    /// Gets the expiration time in minutes for the access token.
+    /// </summary>
     public int AccessTokenExpirationMinutes { get; init; }
+
+    /// <summary>
+    /// Gets the expiration time in minutes for the refresh token.
+    /// </summary>
     public int RefreshTokenExpirationMinutes { get; init; }
 }
--- src/GSWCloudApp.Common/Identity/Options/SecurityOptions.cs
+++ src/GSWCloudApp.Common/Identity/Options/SecurityOptions.cs
@@ -1,18 +1,62 @@
 ﻿namespace GSWCloudApp.Common.Identity.Options;
 
+/// <summary>
+/// Represents the security options for user authentication and authorization.
+/// </summary>
 public class SecurityOptions
 {
+    /// <summary>
+    /// Gets a value indicating whether a digit is required in the password.
+    /// </summary>
     public bool RequireDigit { get; init; } = true;
+
+    /// <summary>
+    /// Gets the required length of the password.
+    /// </summary>
     public int RequireLenght { get; init; } = 8;
+
+    /// <summary>
+    /// Gets a value indicating whether an uppercase letter is required in the password.
+    /// </summary>
     public bool RequireUppercase { get; init; } = true;
+
+    /// <summary>
+    /// Gets a value indicating whether a lowercase letter is required in the password.
+    /// </summary>
     public bool RequireLowercase { get; init; } = true;
+
+    /// <summary>
+    /// Gets a value indicating whether a non-alphanumeric character is required in the password.
+    /// </summary>
     public bool RequireNonAlphanumeric { get; init; } = true;
+
+    /// <summary>
+    /// Gets the number of unique characters required in the password.
+    /// </summary>
     public int RequireUniqueChars { get; init; } = 4;
 
+    /// <summary>
+    /// Gets a value indicating whether a unique email is required for each user.
+    /// </summary>
     public bool RequireUniqueEmail { get; init; } = true;
+
+    /// <summary>
+    /// Gets a value indicating whether email confirmation is required for user registration.
+    /// </summary>
     public bool RequireConfirmedEmail { get; init; } = true;
 
+    /// <summary>
+    /// Gets a value indicating whether new users are allowed.
+    /// </summary>
     public bool AllowedForNewUsers { get; init; } = true;
+
+    /// <summary>
+    /// Gets the maximum number of failed access attempts before a user is locked out.
+    /// </summary>
     public int MaxFailedAccessAttempts { get; init; } = 5;
+
+    /// <summary>
+    /// Gets the default lockout time span after the maximum number of failed access attempts is reached.
+    /// </summary>
     public TimeSpan DefaultLockoutTimeSpan { get; init; } = TimeSpan.FromMinutes(5);
 }
