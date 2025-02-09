32d916eb9db11122dd55fbb24a09109919d3dceb
Angelo Pirola <angelo9871@gmail.com>
2025-02-09 01:05:11 +0100

    Refactoring Identity Common class library

--- src/GSWCloudApp.Common/Identity/SecurityDbContext.cs
+++ src/GSWCloudApp.Common/Identity/SecurityDbContext.cs
@@ -5,6 +5,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GSWCloudApp.Common.Identity;
 
+/// <summary>
+/// Represents the database context for security-related entities, including users, roles, and their claims.
+/// </summary>
+/// <param name="options">The options to be used by a <see cref="DbContext"/>.</param>
 public class SecurityDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser, ApplicationRole, Guid,
     IdentityUserClaim<Guid>, ApplicationUserRole, IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, IdentityUserToken<Guid>>(options)
 { }
