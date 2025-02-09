e63634d7fbde96cb31cbdc6f41d3396dded2fd07
Angelo Pirola <angelo9871@gmail.com>
2025-02-09 01:05:30 +0100

    Aggiunto nuovo Helpers Common class library

---
+++ src/GSWCloudApp.Common/Helpers/UsersHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace GSWCloudApp.Common.Helpers;
+
+public static class UsersHelpers
+{
+    /// <summary>
+    /// Retrieves the user ID from the current HTTP context.
+    /// </summary>
+    /// <param name="httpContextAccessor">The HTTP context accessor to retrieve the current HTTP context.</param>
+    /// <returns>The user ID as a <see cref="Guid"/>. Returns <see cref="Guid.Empty"/> if the user ID is not found.</returns>
+    public static Guid GetUserId(IHttpContextAccessor httpContextAccessor)
+    {
+        var guid = httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var userId = guid != null ? Guid.Parse(guid) : Guid.Empty;
+
+        return userId;
+    }
+}
