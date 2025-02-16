433e6a3080b94ce5cec6ee0eb61a4f7490fb536d
Angelo Pirola <angelo9871@gmail.com>
2025-02-16 09:47:12 +0100

    Update project to .NET 8.0 and enhance configuration
    - Updated `ConfigurazioniSvc.BusinessLayer.csproj` to target .NET 8.0, enabling implicit usings and nullable reference types, and added a reference to `ConfigurazioniSvc.Shared`.
    - Introduced `ConfigurationAppExtensions` class in `ConfigurationAppExtensions.cs` with methods to regenerate and generate JSON configuration files, utilizing various application settings and custom serialization options.

---
+++ src/ConfigurazioniSvc/ConfigurazioniSvc.BusinessLayer/Extensions/ConfigurationAppExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.Json;
+using ConfigurazioniSvc.Shared;
+using GSWCloudApp.Common.Identity.Options;
+using GSWCloudApp.Common.Options;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigurazioniSvc.BusinessLayer.Extensions;
+
+public static class ConfigurationAppExtensions
+{
+    public static void RigenerateJSON(IConfiguration configuration, string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+
+        GenerateJSON(configuration, filePath);
+    }
+
+    public static void GenerateJSON(IConfiguration configuration, string filePath)
+    {
+        var applicationOptions = configuration.GetSection("ApplicationOptions").Get<ApplicationOptions>() ?? new();
+        var jwtSettings = configuration.GetSection("JwtOptions").Get<JwtOptions>() ?? new();
+        var workerSettings = configuration.GetSection("WorkerSettings").Get<WorkerSettings>() ?? new();
+        var pollyPolicyOptions = configuration.GetSection("PollyPolicyOptions").Get<PollyPolicyOptions>() ?? new();
+
+        var configurationApp = new ConfigurationApp()
+        {
+            ConnectionStrings = new ConnectionStrings()
+            {
+                SqlAutentica = configuration.GetConnectionString("SqlAutentica") ?? string.Empty,
+                SqlConfigSmtp = configuration.GetConnectionString("SqlConfigSmtp") ?? string.Empty,
+                SqlGestDocumenti = configuration.GetConnectionString("SqlGestDocumenti") ?? string.Empty,
+                SqlGestLoghi = configuration.GetConnectionString("SqlGestLoghi") ?? string.Empty,
+                SqlInvioEmail = configuration.GetConnectionString("SqlInvioEmail") ?? string.Empty
+            },
+            ApplicationOptions = new ApplicationOptions()
+            {
+                TabellaMigrazioni = applicationOptions.TabellaMigrazioni,
+                SwaggerEnable = applicationOptions.SwaggerEnable,
+                MaxRetryCount = applicationOptions.MaxRetryCount,
+                MaxRetryDelaySeconds = applicationOptions.MaxRetryDelaySeconds
+            },
+            JwtOptions = new JwtOptions()
+            {
+                Audience = jwtSettings.Audience,
+                Issuer = jwtSettings.Issuer,
+                SecurityKey = jwtSettings.SecurityKey,
+                AccessTokenExpirationMinutes = jwtSettings.AccessTokenExpirationMinutes,
+                RefreshTokenExpirationMinutes = jwtSettings.RefreshTokenExpirationMinutes
+            },
+            WorkerSettings = new WorkerSettings()
+            {
+                WorkerDelayInSeconds = workerSettings.WorkerDelayInSeconds
+            },
+            PollyPolicyOptions = new PollyPolicyOptions()
+            {
+                RetryCount = pollyPolicyOptions.RetryCount,
+                SleepDuration = pollyPolicyOptions.SleepDuration
+            },
+            DefaultAdminPassword = configuration.GetSection("DefaultAdminPassword").Value ?? string.Empty
+        };
+
+        File.WriteAllText(filePath, JsonSerializer.Serialize(configurationApp, jsonOptions));
+    }
+
+    private static readonly JsonSerializerOptions jsonOptions = new JsonSerializerOptions
+    {
+        IndentSize = 2,
+        PropertyNameCaseInsensitive = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = true
+    };
+}
