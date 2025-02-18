73084dbf4f5cdfe234be35f5e7f2c728e86db7dd
Angelo Pirola <angelo9871@gmail.com>
2025-02-18 09:00:07 +0100

    Add configuration and settings classes
    Introduce new classes for application configuration, connection strings, Polly policy options, and worker settings. Each class includes relevant properties initialized to default values, enhancing the application's configuration management capabilities.

---
+++ src/GSWCloudApp.Common/Options/WorkerSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace GSWCloudApp.Common.Options;
+
+/// <summary>
+/// Represents the settings for the worker.
+/// </summary>
+public class WorkerSettings
+{
+    /// <summary>
+    /// Gets or sets the delay in seconds for the worker.
+    /// </summary>
+    public int WorkerDelayInSeconds { get; set; }
+}
