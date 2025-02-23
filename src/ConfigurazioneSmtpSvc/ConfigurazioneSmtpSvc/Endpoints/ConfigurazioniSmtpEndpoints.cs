c9e680c7eae69ef9c0388ae630750734ad7e49ff
Angelo Pirola <angelo9871@gmail.com>
2025-02-23 10:45:44 +0100

    Update project to .NET 8.0 and enhance configurations
    - Updated `ConfigurazioneSmtpSvc.csproj` to target .NET 8.0, added properties for nullable reference types, implicit usings, and user secrets, along with a package reference for `Microsoft.EntityFrameworkCore.Tools`.
    - Modified `Dockerfile` to use .NET 8.0 images, updated working directories, and adjusted build and publish commands.
    - Added new endpoint mappings in `ConfigurazioniSenderEndpoints.cs` and `ConfigurazioniSmtpEndpoints.cs` for managing sender and SMTP configurations, including validation and OpenAPI documentation.
    - Enhanced `Program.cs` to configure services, middleware, and application settings, including logging, CORS, API versioning, and Swagger documentation.
    - Added `launchSettings.json` for application launch profile and created `appsettings.Development.json` for development logging configuration.
    - Updated `appsettings.json` to include Serilog configuration for logging to console, file, and MongoDB, along with Kestrel server settings.

---
+++ src/ConfigurazioneSmtpSvc/ConfigurazioneSmtpSvc/Endpoints/ConfigurazioniSmtpEndpoints.cs
@@ -0,0 +1,100 @@
+﻿using ConfigurazioneSmtpSvc.BusinessLayer.Services.Interfaces;
+using ConfigurazioneSmtpSvc.Shared.DTO.SettingSmtp;
+using GSWCloudApp.Common.Constants;
+using GSWCloudApp.Common.Routing;
+using GSWCloudApp.Common.Validation.Extensions;
+using Microsoft.OpenApi.Models;
+
+namespace ConfigurazioneSmtpSvc.Endpoints;
+
+public class ConfigurazioniSmtpEndpoints : IEndpointRouteHandlerBuilder
+{
+    public static void MapEndpoints(IEndpointRouteBuilder endpoints)
+    {
+        var apiService = endpoints.ServiceProvider.GetRequiredService<IConfigurazioneService>();
+
+        var apiGroup = endpoints
+            .MapGroup("/configurazionismtp")
+            .MapToApiVersion(1)
+            .WithOpenApi(opt =>
+            {
+                opt.Tags = [new OpenApiTag { Name = "Configurazioni Smtp" }];
+
+                return opt;
+            });
+
+        //apiGroup.MapGet(MinimalApi.PatternEmpty, async Task<Results<Ok<List<SettingSmtpDto>>, BadRequest>> ([FromServices] ISender sender,
+        //    CancellationToken cancellationToken) =>
+        //{
+        //    var result = await sender.Send(new GetAllSettingSmtpQuery(), cancellationToken);
+
+        //    if (result != null)
+        //    {
+        //        return TypedResults.Ok(result);
+        //    }
+
+        //    return TypedResults.BadRequest();
+        //})
+        apiGroup.MapGet(MinimalApi.PatternEmpty, apiService.GetSmtpConfigurationAsync)
+            .Produces<IQueryable<SettingSmtpDto>>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .WithOpenApi(opt =>
+            {
+                opt.Summary = "Get all smtp configurations";
+                opt.Description = "Extracts all smtp configurations present in the database";
+
+                return opt;
+            });
+
+        //apiGroup.MapPost(MinimalApi.PatternEmpty, async Task<Results<Ok<SettingSmtpDto>, BadRequest>> (CreateSettingSmtpDto request,
+        //    ISender sender, CancellationToken cancellationToken) =>
+        //{
+        //    var result = await sender.Send(new CreateSettingSmtpCommand(request), cancellationToken);
+
+        //    if (result != null)
+        //    {
+        //        return TypedResults.Ok(result);
+        //    }
+
+        //    return TypedResults.BadRequest();
+        //})
+        apiGroup.MapPost(MinimalApi.PatternEmpty, apiService.CreateSmtpConfigurationAsync)
+            .Produces<SettingSmtpDto>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .ProducesProblem(StatusCodes.Status422UnprocessableEntity)
+            .WithValidation<CreateSettingSmtpDto>()
+            .WithOpenApi(opt =>
+            {
+                opt.Summary = "Create a new smtp configurations";
+                opt.Description = "Create a new smtp configurations";
+
+                return opt;
+            });
+
+        //apiGroup.MapPatch(MinimalApi.PatternEmpty, async Task<Results<Ok<SettingSmtpDto>, BadRequest>> (EditSettingSmtpDto request, ISender sender,
+        //    CancellationToken cancellationToken) =>
+        //{
+        //    var result = await sender.Send(new EditSettingSmtpCommand(request), cancellationToken);
+
+        //    if (result != null)
+        //    {
+        //        return TypedResults.Ok(result);
+        //    }
+
+        //    return TypedResults.BadRequest();
+        //})
+        apiGroup.MapPatch(MinimalApi.PatternEmpty, apiService.EditSmtpConfigurationAsync)
+            .Produces<SettingSmtpDto>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .WithOpenApi(opt =>
+            {
+                opt.Summary = "Edit a smtp configurations";
+                opt.Description = "Edit a smtp configurations";
+
+                return opt;
+            });
+    }
+}
---
+++ src/ConfigurazioneSmtpSvc/ConfigurazioneSmtpSvc/Program.cs
@@ -0,0 +1,63 @@
+using ConfigurazioneSmtpSvc.BusinessLayer.Mediator.Handlers.GetAll;
+using ConfigurazioneSmtpSvc.BusinessLayer.Services;
+using ConfigurazioneSmtpSvc.BusinessLayer.Services.Interfaces;
+using ConfigurazioneSmtpSvc.BusinessLayer.Validator;
+using ConfigurazioneSmtpSvc.DataAccessLayer;
+using GSWCloudApp.Common;
+using GSWCloudApp.Common.Extensions;
+using GSWCloudApp.Common.Helpers;
+using GSWCloudApp.Common.Routing;
+using BLConstants = GSWCloudApp.Common.Constants.BusinessLayer;
+
+namespace ConfigurazioneSmtpSvc;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var applicationOptions = await MicroservicesExtensions.GetApplicationOptionsAsync(builder.Configuration);
+        var databaseConnection = await MicroservicesExtensions.GetConnectionStringFromNamingAsync(builder.Configuration, "SqlConfigSmtp");
+
+        ServiceExtensions.AddConfigurationSerilog<Program>(builder);
+
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.ConfigureJsonOptions();
+
+        builder.Services.ConfigureDbContextAsync<Program, AppDbContext>(applicationOptions, databaseConnection);
+        builder.Services.ConfigureCors(BLConstants.DefaultCorsPolicyName);
+
+        builder.Services.ConfigureApiVersioning();
+        builder.Services.ConfigureSwagger();
+
+        builder.Services.AddAntiforgery();
+        builder.Services.AddTransient<IConfigurazioneService, ConfigurazioneService>();
+
+        builder.Services.ConfigureMediator<GetAllSettingSmtpHandler>();
+        builder.Services.ConfigureGenericServices();
+
+        builder.Services.ConfigureProblemDetails();
+        builder.Services.ConfigureFluentValidation<CreateSettingSenderValidator>();
+
+        builder.Services.ConfigureOptions(builder.Configuration);
+
+        var app = builder.Build();
+        var versionedApi = ApplicationExtensions.UseVersioningApi(app);
+
+        await app.ApplyMigrationsAsync<AppDbContext>();
+        app.UseExceptionHandler();
+
+        app.UseStatusCodePages();
+        app.UseDevSwagger(applicationOptions);
+
+        app.UseForwardNetworking();
+        app.UseRouting();
+
+        app.UseCors(BLConstants.DefaultCorsPolicyName);
+        app.UseAntiforgery();
+
+        versionedApi.MapEndpoints();
+        await app.RunAsync();
+    }
+}
