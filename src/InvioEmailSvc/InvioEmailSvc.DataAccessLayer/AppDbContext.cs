97d05265c8528e89d0275dd46fb7393413e03d2d
Angelo Pirola <angelo9871@gmail.com>
2025-03-02 13:36:47 +0100

    Add data access layer for email service
    Introduce AppDbContext for managing EmailMessage and EmailOutboxMessage entities. Implement entity configurations with soft deletion filters. Update project to target .NET 8.0 and enable nullable reference types.

---
+++ src/InvioEmailSvc/InvioEmailSvc.DataAccessLayer/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using InvioEmailSvc.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvioEmailSvc.DataAccessLayer;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<EmailMessage> EmailMessages { get; set; } = null!;
+    public DbSet<EmailOutboxMessage> EmailOutboxMessages { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ src/InvioEmailSvc/InvioEmailSvc.DataAccessLayer/Extension/EmailMessageExtension.cs
@@ -0,0 +1,16 @@
+﻿using GSWCloudApp.Common.Identity.Entities;
+using InvioEmailSvc.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InvioEmailSvc.DataAccessLayer.Extension;
+
+public class EmailMessageExtension : BaseEntityConfiguration<EmailMessage>
+{
+    public override void Configure(EntityTypeBuilder<EmailMessage> builder)
+    {
+        builder.ToTable("InvioEmail");
+
+        builder.HasQueryFilter(builder => builder.DeletedDateTime == null);
+    }
+}
