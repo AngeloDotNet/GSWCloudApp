8c61f43201768306f182798a0141a6590362a17d
Angelo Pirola <angelo9871@gmail.com>
2025-05-04 09:50:24 +0200

    Refactor email mapping logic
    Removed MappingProfile class and its mappings.
    Introduced ProfileMapper with a method to convert
    CreateEmailMessageCommand to EmailMessage.

---
+++ src/InvioEmailSvc/InvioEmailSvc.BusinessLayer/Mapper/ProfileMapper.cs
@@ -0,0 +1,20 @@
+﻿using InvioEmailSvc.BusinessLayer.Mediator.Command;
+using InvioEmailSvc.DataAccessLayer.Entities;
+
+namespace InvioEmailSvc.BusinessLayer.Mapper;
+
+public static class ProfileMapper
+{
+    public static EmailMessage CreateEmailMessageCommandToEntity(this CreateEmailMessageCommand command)
+    {
+        return new EmailMessage
+        {
+            To = command.To,
+            Cc = command.Cc,
+            Bcc = command.Bcc,
+            ReplyTo = command.ReplyTo,
+            Subject = command.Subject,
+            TextContent = command.TextContent
+        };
+    }
+}
