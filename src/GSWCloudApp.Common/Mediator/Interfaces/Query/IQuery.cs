21d9925b99d5139b2c5cd1acd2ceec441c987374
Angelo Pirola <angelo9871@gmail.com>
2025-05-18 10:21:55 +0200

    Refactor IQuery and IQueryHandler interfaces
    Removed MediatR dependency from IQuery interface, simplifying it to just IQuery<TResponse>. Updated IQueryHandler to include a Handle method for processing queries, enhancing its functionality while also eliminating the MediatR dependency.

--- src/GSWCloudApp.Common/Mediator/Interfaces/Query/IQuery.cs
+++ src/GSWCloudApp.Common/Mediator/Interfaces/Query/IQuery.cs
@@ -1,10 +1,4 @@
-﻿using MediatR;
+﻿namespace GSWCloudApp.Common.Mediator.Interfaces.Query;
 
-namespace GSWCloudApp.Common.Mediator.Interfaces.Query;
-
-/// <summary>
-/// Represents a query with a response type that implements the MediatR IRequest interface.
-/// </summary>
-/// <typeparam name="TResponse">The type of the response.</typeparam>
-public interface IQuery<TResponse> : IRequest<TResponse>
+public interface IQuery<TResponse>
 { }
--- src/GSWCloudApp.Common/Mediator/Interfaces/Query/IQueryHandler.cs
+++ src/GSWCloudApp.Common/Mediator/Interfaces/Query/IQueryHandler.cs
@@ -1,11 +1,6 @@
-﻿using MediatR;
+﻿namespace GSWCloudApp.Common.Mediator.Interfaces.Query;
 
-namespace GSWCloudApp.Common.Mediator.Interfaces.Query;
-
-/// <summary>
-/// Defines a handler for a query that returns a response.
-/// </summary>
-/// <typeparam name="TQuery">The type of the query.</typeparam>
-/// <typeparam name="TResponse">The type of the response.</typeparam>
-public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, TResponse> where TQuery : IQuery<TResponse>
-{ }
+public interface IQueryHandler<in TQuery, TResponse> where TQuery : IQuery<TResponse>
+{
+    Task<TResponse> Handle(TQuery query, CancellationToken cancellationToken);
+}
