e392b8e61ff62b683a71a8d62990667d89d5ce7f
Angelo Pirola <angelo9871@gmail.com>
2025-05-23 08:57:08 +0200

    Refactor email service to use command handler
    Updated ISendEmailService to simplify SendEmailAsync method by removing the ISender parameter.
    Modified SendEmailService to utilize ICommandHandler for command handling, enhancing the architecture towards a command-oriented approach.
    Refactored using directives to clean up dependencies, removing unnecessary imports.

--- src/InvioEmailSvc/InvioEmailSvc.BusinessLayer/Services/SendEmailService.cs
+++ src/InvioEmailSvc/InvioEmailSvc.BusinessLayer/Services/SendEmailService.cs
@@ -1,17 +1,17 @@
-﻿using InvioEmailSvc.BusinessLayer.Mediator.Command;
+﻿using GSWCloudApp.Common.Mediator.Interfaces.Command;
+using InvioEmailSvc.BusinessLayer.Mediator.Command;
 using InvioEmailSvc.BusinessLayer.Services.Interfaces;
 using InvioEmailSvc.Shared.DTO;
-using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace InvioEmailSvc.BusinessLayer.Services;
 
-public class SendEmailService : ISendEmailService
+public class SendEmailService(ICommandHandler<CreateEmailMessageCommand, bool> handler) : ISendEmailService
 {
-    public async Task<Results<Ok, BadRequest>> SendEmailAsync(CreateEmailMessageDto request, ISender sender, CancellationToken cancellationToken = default)
+    public async Task<Results<Ok, BadRequest>> SendEmailAsync(CreateEmailMessageDto request, CancellationToken cancellationToken)
     {
-        return await sender.Send(new CreateEmailMessageCommand(request), cancellationToken) switch
+        return await handler.Handle(new CreateEmailMessageCommand(request), cancellationToken) switch
         {
             true => TypedResults.Ok(),
             _ => TypedResults.BadRequest()
