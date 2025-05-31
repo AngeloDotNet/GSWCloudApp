a1eaeab838f23cef29510407dbfc9f8e7283f61b
Angelo Pirola <angelo9871@gmail.com>
2025-05-31 10:24:48 +0200

    Refactor document upload mapping and validation
    - Removed `MappingProfile.cs` and replaced it with `ProfileMapper.cs` for mapping `CreateDocumentoDto` to `Documento`.
    - Simplified `IUploadService` by removing `IMapper` from method signatures.
    - Updated `UploadService` to utilize `ProfileMapper` instead of `IMapper`.
    - Added `UploadDocumentoValidator` for validating `UploadDocumentoDto` with FluentValidation.

--- src/GestioneDocumentiSvc/GestioneDocumentiSvc.BusinessLayer/Services/IUploadService.cs
+++ src/GestioneDocumentiSvc/GestioneDocumentiSvc.BusinessLayer/Services/IUploadService.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using GestioneDocumentiSvc.DataAccessLayer;
+﻿using GestioneDocumentiSvc.DataAccessLayer;
 using GestioneDocumentiSvc.Shared.DTO;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -8,7 +7,7 @@ namespace GestioneDocumentiSvc.BusinessLayer.Services;
 
 public interface IUploadService
 {
-    Task<Results<Ok, BadRequest<string>, Conflict<string>>> UploadFileAsync([FromForm] UploadDocumentoDto documentoDto, IMapper mapper, AppDbContext dbContext);
-    Task<Results<FileContentHttpResult, NotFound<string>>> DownloadFileAsync(string fileName, IMapper mapper, AppDbContext dbContext);
+    Task<Results<Ok, BadRequest<string>, Conflict<string>>> UploadFileAsync([FromForm] UploadDocumentoDto documentoDto, AppDbContext dbContext);
+    Task<Results<FileContentHttpResult, NotFound<string>>> DownloadFileAsync(string fileName, AppDbContext dbContext);
     Task<Results<NoContent, NotFound<string>>> DeleteFileAsync(string fileName, AppDbContext dbContext);
 }
