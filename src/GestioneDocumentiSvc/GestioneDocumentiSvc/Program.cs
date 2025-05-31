228a4160b813d5337ec8768707aa845b5c781518
Angelo Pirola <angelo9871@gmail.com>
2025-05-31 10:24:56 +0200

    Update dependencies and refactor configuration
    - Bump `Microsoft.EntityFrameworkCore.Tools` to version 9.0.5.
    - Replace using directives in `Program.cs` for new common extensions.
    - Refactor `Main` method to utilize updated methods for app options and DB connection.
    - Comment out `iisSettings` in `launchSettings.json`.
    - Remove MongoDB logging configuration and connection strings from `appsettings.json`.

--- src/GestioneDocumentiSvc/GestioneDocumentiSvc/Program.cs
+++ src/GestioneDocumentiSvc/GestioneDocumentiSvc/Program.cs
@@ -1,7 +1,7 @@
-using GestioneDocumentiSvc.BusinessLayer.Mapper;
 using GestioneDocumentiSvc.BusinessLayer.Services;
 using GestioneDocumentiSvc.BusinessLayer.Validation;
 using GestioneDocumentiSvc.DataAccessLayer;
+using GSWCloudApp.Common;
 using GSWCloudApp.Common.Extensions;
 using GSWCloudApp.Common.Helpers;
 using GSWCloudApp.Common.Routing;
@@ -15,25 +15,22 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        var applicationOptions = await MicroservicesExtensions.GetApplicationOptionsAsync(builder.Configuration);
+        var databaseConnection = await MicroservicesExtensions.GetConnectionStringFromNamingAsync(builder.Configuration, "SqlGestDocumenti");
+
         ServiceExtensions.AddConfigurationSerilog<Program>(builder);
 
         builder.Services.AddHttpContextAccessor();
         builder.Services.ConfigureJsonOptions();
 
-        var postgresConnection = IConfigurationExtensions.GetDatabaseConnection(builder, "SqlGestDocumenti");
-        var appOptions = IConfigurationExtensions.GetApplicationOptions(builder);
-
-        builder.Services.ConfigureDbContextAsync<Program, AppDbContext>(postgresConnection, appOptions);
-        builder.Services.ConfigureCors(BLConstants.DefaultCorsPolicyName);
-
-        builder.Services.ConfigureApiVersioning();
-        builder.Services.ConfigureSwagger();
+        builder.Services.ConfigureDbContextAsync<Program, AppDbContext>(applicationOptions, databaseConnection);
+        builder.Services.AddDefaultServices(BLConstants.DefaultCorsPolicyName);
 
         builder.Services.AddAntiforgery();
         builder.Services.AddTransient<IUploadService, UploadService>();
 
         builder.Services.ConfigureProblemDetails();
-        builder.Services.ConfigureServicesNoCaching<MappingProfile, CreateDocumentoValidator>();
+        builder.Services.ConfigureFluentValidation<CreateDocumentoValidator>();
 
         builder.Services.ConfigureOptions(builder.Configuration);
 
@@ -41,15 +38,11 @@ public class Program
         var versionedApi = ApplicationExtensions.UseVersioningApi(app);
 
         await app.ApplyMigrationsAsync<AppDbContext>();
+
         app.UseExceptionHandler();
-
         app.UseStatusCodePages();
-        app.UseDevSwagger(appOptions);
 
-        app.UseForwardNetworking();
-        app.UseRouting();
-
-        app.UseCors(BLConstants.DefaultCorsPolicyName);
+        app.UseDefaultServices(applicationOptions, BLConstants.DefaultCorsPolicyName);
         app.UseAntiforgery();
 
         versionedApi.MapEndpoints();
