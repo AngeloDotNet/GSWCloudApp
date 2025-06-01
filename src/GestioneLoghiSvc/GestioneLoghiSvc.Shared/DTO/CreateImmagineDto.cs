27be7416f16aca1813a396868d8711c49839b527
Angelo Pirola <angelo9871@gmail.com>
2025-06-01 10:05:08 +0200

    Add new DTOs and update project to .NET 8.0
    Introduced `CreateImmagineDto`, `ImmagineDto`, and `UploadImmagineDto` in the `GestioneLoghiSvc.Shared.DTO` namespace, defining essential properties for image management. Updated `GestioneLoghiSvc.Shared.csproj` to target .NET 8.0, enabling implicit usings and nullable reference types, and added a project reference to `GSWCloudApp.Common`.

---
+++ src/GestioneLoghiSvc/GestioneLoghiSvc.Shared/DTO/CreateImmagineDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GestioneLoghiSvc.Shared.DTO;
+
+public class CreateImmagineDto
+{
+    public Guid FestaId { get; set; }
+    public string Path { get; set; } = null!;
+    public string ContentType { get; set; } = null!;
+    public string Extension { get; set; } = null!;
+    public long Length { get; set; }
+    public string NomeImmagine { get; set; } = null!;
+    public string Descrizione { get; set; } = null!;
+}
