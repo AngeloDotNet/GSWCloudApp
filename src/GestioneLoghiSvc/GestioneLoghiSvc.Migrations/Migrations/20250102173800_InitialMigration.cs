26d699aefca1f30e78b1ea61079ff5b8794b4168
Angelo Pirola <angelo9871@gmail.com>
2025-06-01 10:04:58 +0200

    Update project to .NET 8.0 and add Immagini migration
    - Updated `GestioneLoghiSvc.Migrations.csproj` to target .NET 8.0, enable implicit usings, and nullable reference types. Added project reference to `GestioneLoghiSvc.DataAccessLayer`.
    - Modified `20250102173800_InitialMigration.Designer.cs` to define the `Immagini` entity model with necessary properties and database annotations.
    - Created `20250102173800_InitialMigration.cs` to implement the migration for the `Immagini` table, detailing columns and primary key.
    - Updated `AppDbContextModelSnapshot.cs` to reflect the current state of the `Immagini` entity model.

---
+++ src/GestioneLoghiSvc/GestioneLoghiSvc.Migrations/Migrations/20250102173800_InitialMigration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestioneLoghiSvc.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Immagini",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FestaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Path = table.Column<string>(type: "text", nullable: false),
+                    ContentType = table.Column<string>(type: "text", nullable: false),
+                    Extension = table.Column<string>(type: "text", nullable: false),
+                    Length = table.Column<long>(type: "bigint", nullable: false),
+                    NomeImmagine = table.Column<string>(type: "text", nullable: false),
+                    Descrizione = table.Column<string>(type: "text", nullable: false),
+                    CreatedByUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedByUserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ModifiedDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedByUserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    DeletedDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Immagini", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Immagini");
+        }
+    }
+}
