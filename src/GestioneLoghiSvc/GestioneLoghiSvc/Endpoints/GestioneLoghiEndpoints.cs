bf6948fffea9b19a84e482cb04961239a9ffaaad
Angelo Pirola <angelo9871@gmail.com>
2025-06-01 10:04:03 +0200

    Update project to .NET 8.0 and enhance endpoints
    - Updated Dockerfile to use .NET 8.0 for base and build images.
    - Added new endpoint mappings in `GestioneLoghiEndpoints.cs` for image uploads, downloads, and deletions with OpenAPI documentation.
    - Modified `GestioneLoghiSvc.csproj` to target .NET 8.0 and include necessary package and project references.
    - Enhanced `Program.cs` with new services, JSON configuration, and middleware for error handling.
    - Updated `launchSettings.json` to define application profile settings.
    - Added `appsettings.Development.json` for development logging configuration.
    - Updated `appsettings.json` to include Serilog logging setup and Kestrel request limits.

---
+++ src/GestioneLoghiSvc/GestioneLoghiSvc/Endpoints/GestioneLoghiEndpoints.cs
@@ -0,0 +1,65 @@
+﻿using GestioneLoghiSvc.BusinessLayer.Services;
+using GestioneLoghiSvc.Shared.DTO;
+using GSWCloudApp.Common.Constants;
+using GSWCloudApp.Common.Routing;
+using GSWCloudApp.Common.Validation.Extensions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.OpenApi.Models;
+
+namespace GestioneLoghiSvc.Endpoints;
+
+public class GestioneLoghiEndpoints : IEndpointRouteHandlerBuilder
+{
+    public static void MapEndpoints(IEndpointRouteBuilder endpoints)
+    {
+        var apiService = endpoints.ServiceProvider.GetRequiredService<IUploadService>();
+
+        var apiGroup = endpoints
+            .MapGroup("/immagini")
+            .MapToApiVersion(1)
+            .WithOpenApi(opt =>
+            {
+                opt.Tags = [new OpenApiTag { Name = "Gestione Immagini" }];
+
+                return opt;
+            })
+            .DisableAntiforgery();
+
+        apiGroup.MapPost(MinimalApi.PatternUpload, apiService.UploadFileAsync)
+            .Produces(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .ProducesProblem(StatusCodes.Status422UnprocessableEntity)
+            .WithValidation<UploadImmagineDto>()
+            .WithOpenApi(opt =>
+            {
+                opt.Summary = "Upload image to server";
+                opt.Description = "Upload image to server";
+
+                return opt;
+            });
+
+        apiGroup.MapGet(MinimalApi.PatternDownload, apiService.DownloadFileAsync)
+            .Produces<FileStreamHttpResult>()
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithOpenApi(opt =>
+            {
+                opt.Summary = "Download image from server";
+                opt.Description = "Download image from server";
+
+                return opt;
+            });
+
+        apiGroup.MapDelete(MinimalApi.PatternDeleteDocument, apiService.DeleteFileAsync)
+            .Produces(StatusCodes.Status204NoContent)
+            .ProducesProblem(StatusCodes.Status401Unauthorized)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithOpenApi(opt =>
+            {
+                opt.Summary = "Delete image from server";
+                opt.Description = "Delete image from server";
+                return opt;
+            });
+    }
+}
